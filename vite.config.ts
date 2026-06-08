ba372d92302f371c37877706a5a3e6632431f238
MaxiResults <maxiresults.digital@gmail.com>
2026-06-08 08:27:50 -0300

    fix: terser com keep_fnames para resolver bug de minificação do SWC no Conversas

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,19 @@ import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        keep_classnames: true,
+        keep_fnames: true,
+      },
+      mangle: {
+        keep_classnames: true,
+        keep_fnames: true,
+      },
+    },
+  },
   server: {
     host: "::",
     port: 8080,
